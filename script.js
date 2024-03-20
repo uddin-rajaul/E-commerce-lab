7eb771757a11720002c926a9a669448822b69448
Swaraj Barnwal <ikku12piku@gmail.com>
2024-03-21 00:48:01 +0545

    Fixed - eye icon.

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 // Select the eye icon which is used to toggle the password visibility
-const showPasswordButton = document.querySelector("#show-password");
+const showPasswordButton = document.querySelector("#show-password-btn");
+const passwordField = document.querySelector("#password");
 
+showPasswordButton.addEventListener("click", function () {
+  // showPasswordButton.style.display = "none";
+  // hidePasswordButton.style.display = "block";
+  this.classList.toggle("fa-eye-slash");
 
-const passwordField = document.querySelector("#show-password");
-
-showPassword.addEventListener("click", function () {
-    this.classList.toggle("fa-solid");
-  
-    const type = passwordField.getAttribute("type") === "password" ? "text" : "password";
-    passwordField.setAttribute("type", type);
+  const type =
+    passwordField.getAttribute("type") === "password" ? "text" : "password";
+  passwordField.setAttribute("type", type);
 });
